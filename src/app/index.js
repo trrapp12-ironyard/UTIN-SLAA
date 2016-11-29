b1521705a95c7bdc3910007c083383f0036cd7e8
trrapp@gmail.com <trrapp@gmail.com>
2016-11-28 18:35:04 -0700

    fix index.js reference from query selector to elementById and create a const to massage data

--- src/app/index.js
+++ src/app/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
 
-render(<App/>, document.querySelector('#groupParentInformation'));
+const app = document.getElementById('groupParentInformation')
+render(<App/>, app);
 // render(<App />, app);  This is how it looked in Kelly's
