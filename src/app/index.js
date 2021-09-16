482b399bee0b0bd9e6819ac9c5f16a6ed77b2945
Trevor Rapp <trrapp@gmail.com>
2021-09-15 23:28:34 -0600

    still fixing

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
-C:\Users\trapp\Desktop\Coding_Projects\UTIN-SLAA\src\\app.js
+
 const app = document.getElementById('groupParentInformation');
 render(<App/>, app);
 // render(<App />, app);  This is how it looked in Kelly's
