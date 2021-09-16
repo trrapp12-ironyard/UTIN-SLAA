c689415c1865fe9ed5c8367ac6ec8d583ed85680
Trevor Rapp <trrapp@gmail.com>
2021-09-15 20:59:01 -0600

    get rid of cannont find module ./components/app.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app.js';
+import App from './app.js';
 
 const app = document.getElementById('groupParentInformation');
 render(<App/>, app);
