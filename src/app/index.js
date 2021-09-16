3c5df13737e5bc19323fbad506d50c3ab35fefc4
Trevor Rapp <trrapp@gmail.com>
2021-09-15 22:36:22 -0600

    still working

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app';
+import App from '../app/components/app';
 
 const app = document.getElementById('groupParentInformation');
 render(<App/>, app);
